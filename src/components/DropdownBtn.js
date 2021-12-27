06028fd0b1cb63eee473480f4d772e91530cc7f8
Tim Hadler <timhadler93@gmail.com>
2021-12-27 16:00:11 +1300

    Componentized dropdown button

---
+++ src/components/DropdownBtn.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function DropdownBtn() {
+    function dbOnClick() {
+        document.getElementById("myDropdown").classList.toggle("show");
+      }
+      
+      // Hide dropdown content when clicked outside of dropdown button
+      window.onclick = function(event) {
+        if (!event.target.matches(".dropbtn")) {
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+          var i;
+          for (i=0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains("show")) {
+              openDropdown.classList.toggle("show");
+            }
+          }
+        }
+      }
+      
+    return (
+        <div className="dropdown">
+        <button onClick={dbOnClick} className="dropbtn">Dropdown</button>
+        <div id ="myDropdown" className="dropdown-content">
+          <p> Item 1 </p>
+          <p> Item 2 </p>
+        </div>
+      </div>
+    );
+}
