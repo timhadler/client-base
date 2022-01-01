58bdf5fe74bd9d47d9cdc48a41f3237679fe0de0
Tim Hadler <timhadler93@gmail.com>
2022-01-01 13:39:45 +1300

    Done customer details top

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ import DetailsForm from "./components/DetailsForm";
 
 function App() {
   const CUSTOMER_DATA = [
-    { id: "1", name: "Anne Barron", contactNum: "0278135441", created: "30/12/2021",  freashAir: "Hrv Gen 1", address: "14 Burwood Rd", suburb: "Burwood", city: "Christchurch", pc: 8043},
-    { id: "2", name: "Barry Stansfield", contactNum: "0278135441", created: "30/12/2021",  freashAir: "Hrv Gen 1", address: "14 Burwood Rd", suburb: "Burwood", city: "Christchurch", pc: 8043}
+    { id: "1", name: "Anne Barron", contactNum: "021 8456 258", eAddress: "barry@stansfield.co.nz", created: "30/12/2021",  freashAir: "Hrv Gen 1", address: "14 Burwood Rd", suburb: "Burwood", city: "Christchurch", pc: 8043, notes:""},
+    { id: "2", name: "Barry Stansfield", contactNum: "021 8456 258", eAddress: "barry@stansfield.co.nz", created: "30/12/2021",  freashAir: "Hrv Gen 1", address: "14 Burwood Rd", suburb: "Burwood", city: "Christchurch", pc: 8043, notes:""}
   ];
 
   const [customers, setCustomers] = useState(CUSTOMER_DATA);
   const customerList = [];
   var i;
   for (i = 0; i < customers.length; i++) {
-    customerList[i] = <li> {customers[i].name} </li>;
+    customerList[i] = <li key={customers[i].id}> {customers[i].name} </li>;
   }
+  const testCustomer = CUSTOMER_DATA[0];
 
   return (
     <div className="App">
@@ -51,7 +52,8 @@ function App() {
           </ul>
         </section>
         <section className="customer_details">
-          <DetailsForm data={customerList[0]} />
+          <DetailsForm customer={testCustomer} />
+          <hr className="line_header_details"/>
         </section>
       </main>
 
--- src/components/DetailsForm.js
+++ src/components/DetailsForm.js
@@ -1,10 +1,61 @@
 import React from "react";
 
 export default function DetailsForm(props) {
+    const customer = props.customer;
+
     return (
-        <div className="customer_detail_top">
-            <h2 className="customer_name"> {props.name} </h2>
-            <button className="button_main"> Edit </button>
+        <div className="customer_details_top">
+            <div className="name-edit">
+                <h2 className="customer_name"> {customer.name} </h2>
+                <button className="button_main button_details_main"> Edit </button>
+            </div>
+            <hr className="line_header_details"/>
+            <div className="details_left">
+                <div className="details_group group_1">
+                    <div className="detail">
+                        <p className="detail_left"> Contact number </p>
+                        <p className="detail_right customer_info"> {customer.contactNum} </p>
+                    </div>
+                    <div className="detail">
+                        <p className="detail_left"> Email address </p>
+                        <p className="detail_right customer_info"> {customer.eAddress} </p>
+                    </div>
+                    <div className="detail">
+                        <p className="detail_left"> Date created </p>
+                        <p className="detail_right customer_info"> {customer.created} </p>
+                    </div>
+                </div>
+                <div className="details_group group_2">
+                    <div className="detail">
+                        <p className="detail_left"> Fresh air system </p>
+                        <p className="detail_right customer_info"> {customer.freashAir} </p>
+                    </div>
+                    <div className="detail">
+                        <p className="detail_left"> Customer notes: </p>
+                        <p className="detail_right customer_info"> {customer.notes} </p>
+                    </div>
+                </div>
+            </div>
+            <div className="details_right">
+                <div className="details_group">
+                    <div className="detail">
+                        <p className="detail_left"> Contact address </p>
+                        <p className="detail_right customer_info"> {customer.address} </p>
+                    </div>
+                    <div className="detail">
+                        <p className="detail_left"> Suburb </p>
+                        <p className="detail_right customer_info"> {customer.suburb} </p>
+                    </div>
+                    <div className="detail">
+                        <p className="detail_left"> City </p>
+                        <p className="detail_right customer_info"> {customer.city} </p>
+                    </div>
+                </div>
+                <div className="detail">
+                        <p className="detail_right customer_info"> Photos(2) </p>
+                </div>
+
+            </div>
         </div>
     )
 }
