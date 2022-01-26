4eaabb1bcd447748453e03885ce6d9345ab8451a
Tim Hadler <timhadler93@gmail.com>
2022-01-26 18:43:04 +1300

    added arrays for each customer section

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,25 @@
-let data = ["One", "Two", "Three"];
+let  data1= ["Anne Brron", "Frederick Botony", "Karrie Ferguson"];
+let  data2= ["Toby Mur", "Nocholous Lous", "Charlie Wort"];
+let  data3= ["Harry Burr", "Barry Stansfield", "Darren Buxley", "Harold Bertrum"];
 
-let list = document.getElementById("myList");
-data.forEach((item) => {
-    let li = document.createElement("li");
-    li.innerText = item;
-    list.appendChild(li);
-});
+let callList = document.getElementById("callList");
+let toBeConfirmed = document.getElementById("toBeConfirmed");
+let confirmed = document.getElementById("confirmed");
+
+setList(callList, data1);
+setList(toBeConfirmed, data2);
+setList(confirmed, data3);
+
+function setList(list, data) {
+    data.forEach((item) => {
+        let li = document.createElement("li");
+        li.innerText = item;
+        li.setAttribute("onClick", "test()")
+        list.appendChild(li);
+    });
+}
+
+function test() {
+    console.log("works");
+    console.log(callList);
+}
