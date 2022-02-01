b650e1fb5091011a13268e1eaaaffff7f55857d3
Tim Hadler <timhadler93@gmail.com>
2022-02-02 12:01:15 +1300

    call list sorted by call dates

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,5 @@
-let  data1= ["Anne Barron", "Frederick Botony", "Karrie Ferguson"];
-let  data2= ["Toby Mur", "Nocholous Lous", "Charlie Wort"];
-let  data3= ["Harry Burr", "Barry Stansfield", "Darren Buxley", "Harold Bertrum"];
-
 // Objects
+// Customer
 function Customer(id, name, cn, ea, add, sub, ci, pc, fa, n, a) {
     this.id = id;
     this.name = name;
@@ -17,19 +14,22 @@ function Customer(id, name, cn, ea, add, sub, ci, pc, fa, n, a) {
     this.action = a;
 }
 
+// Call date
 function CallDate(id, customerId, date) {
     this.id = id;
     this.customerId = customerId;
     this.date = date;
 }
 
+// Customer data for proof-of-concept
+// Object list for customers and call dates
 let customerData = [
     new Customer(1, "Anne Barron", "028 459 2654", "anne@barron.co.nz", "4 Burwood rd", "Burwood", "Christchurch", "8067", "None", "", "callList"),
     new Customer(2, "Frederick Botony", "027 449 3464", "fred@botony.co.nz", "6 Wincroft rd", "Riccarton", "Christchurch", "8065", "HRV Gen1", "", "callList"),
     new Customer(3, "Karrie Ferguson", "021 659 9504", "karrie@furg.co.nz", "13 Oxycotin rd", "Burnside", "Christchurch", "8069", "None", "", "callList"),
-    new Customer(4, "Toby Mur", "021 184 4627", "toby@mur.co.nz", "8 Kin rd", "Burnsdie", "Christchurch", "8089", "None", "", "toBeconfirmed"),
-    new Customer(5, "Nocholous Lous", "021 387 6847", "nic@lous.co.nz", "102 Kindell rd", "Addington", "Christchurch", "8832", "None", "", "toBeconfirmed"),
-    new Customer(6, "Charlie Wort", "021 385 2901", "harlie@wort.co.nz", "56 Oxycotin rd", "Burnside", "Christchurch", "8324", "Unknown", "", "toBeconfirmed"),
+    new Customer(4, "Toby Mur", "021 184 4627", "toby@mur.co.nz", "8 Kin rd", "Burnsdie", "Christchurch", "8089", "None", "", "toBeConfirmed"),
+    new Customer(5, "Nocholous Lous", "021 387 6847", "nic@lous.co.nz", "102 Kindell rd", "Addington", "Christchurch", "8832", "None", "", "toBeConfirmed"),
+    new Customer(6, "Charlie Wort", "021 385 2901", "harlie@wort.co.nz", "56 Oxycotin rd", "Burnside", "Christchurch", "8324", "Unknown", "", "toBeConfirmed"),
     new Customer(7, "Harry Burr", "021 938 3456", "harry@burr.co.nz", "56 Boycotte rd", "Fernside", "Christchurch", "8334", "DVS", "", "confirmed"),
     new Customer(8, "Barry Stansfield", "021 485 6852", "barry@stans.co.nz", "56 Boycotte rd", "Fernside", "Christchurch", "8334", "DVS", "", "confirmed"),
     new Customer(9, "Darren Buxley", "021 789 5214", "darren@bux.co.nz", "201 Foils rd", "Redwood", "Christchurch", "8324", "DVS", "Likes to get DVS serviced every 3 years", "confirmed"),
@@ -38,49 +38,82 @@ let customerData = [
 
 let callData = [
     new CallDate(1, 1, "2022-02-02"),
-    new CallDate(1, 2, "2022-02-04"),
-    new CallDate(1, 3, "2022-02-05"),
-    new CallDate(1, 4, "2022-02-07"),
-    new CallDate(1, 5, "2022-02-09"),
-    new CallDate(1, 6, "2022-02-11"),
-    new CallDate(1, 7, "2022-02-012"),
-    new CallDate(1, 8, "2022-02-12"),
-    new CallDate(1, 9, "2022-02-21"),
-    new CallDate(1, 10, "2022-02-22")
-]
+    new CallDate(8, 8, "2022-02-12"),
+    new CallDate(3, 3, "2022-02-05"),
+    new CallDate(4, 4, "2022-02-07"),
+    new CallDate(5, 5, "2022-02-09"),
+    new CallDate(10, 10, "2022-02-22"),
+    new CallDate(6, 6, "2022-02-11"),
+    new CallDate(7, 7, "2022-02-12"),
+    new CallDate(9, 9, "2022-02-21"),
+    new CallDate(2, 2, "2022-02-04")
+];
+let date = new Date("2022-02-04");
+console.log(callData.sort((a,b) => (a.date > b.date) ? 1 : -1));
 
-let callList = document.getElementById("callList");
-let toBeConfirmed = document.getElementById("toBeConfirmed");
-let confirmed = document.getElementById("confirmed");
 
-setList(callList, data1);
-setList(toBeConfirmed, data2);
-setList(confirmed, data3);
+// setting up html lists
+setLists();
 
-function setList(list, data) {
-    while(list.firstChild) {
-        list.removeChild(list.firstChild);
+function setLists() {
+    var callList = document.getElementById("callList");
+    var toBeConfirmed = document.getElementById("toBeConfirmed");
+    var confirmed = document.getElementById("confirmed");
+
+    // Clear HTML and js lists
+    clearList(callList);
+    clearList(toBeConfirmed);
+    clearList(confirmed);
+
+    // Append all customer names in customerData to the appropriate list, governed by 'action'
+    // Set confirmed and toBeConfirmed lists
+    for (let i = 0; i < customerData.length; i++) {
+        let customer = customerData[i];
+        if (customer.action == "toBeConfirmed") {
+            addCustomerToList(toBeConfirmed, customer);
+        } else if (customer.action == "confirmed") {
+            addCustomerToList(confirmed, customer);
+        }
     }
-    data.forEach((item) => {
-        let li = document.createElement("li");
-        li.innerText = item;
-        //li.setAttribute("onClick", "openCustomerForm()")
-        li.addEventListener('click', function() { openCustomerForm(item); });
-        list.appendChild(li);
-    });
+
+    // Set callBack list sorted by call back date
+    // Customers with multiple call back dates will appear in list mutiple times
+    callData.sort((a,b) => (a.date > b.date) ? 1 : -1);
+    for (let i = 0; i < callData.length; i++) {
+        let call = callData[i];
+        let customer = findCustomer("id", call.customerId);
+        if (customer.action == "callList") {
+            addCustomerToList(callList, customer);
+        }
+    }
 }
 
-function openCustomerForm(name) {
-    let customerDetails = findCustomer(name);
-    document.getElementById("name").innerHTML = customerDetails.name;
-    document.getElementById("cn").innerHTML = customerDetails.contactNumber;
-    document.getElementById("ea").innerHTML = customerDetails.email;
-    document.getElementById("add").innerHTML = customerDetails.address;
-    document.getElementById("sub").innerHTML = customerDetails.suburb;
-    document.getElementById("ci").innerHTML = customerDetails.city;
-    document.getElementById("pc").innerHTML = customerDetails.postCode;
-    document.getElementById("fa").innerHTML = customerDetails.freshAir;
-    document.getElementById("n").innerHTML = customerDetails.notes;
+// creats and appends customer list item to a given list
+function addCustomerToList(list, customer) {
+    let li = document.createElement("li");
+    li.innerHTML = customer.name;
+    li.addEventListener('click', function() { openCustomerForm(customer); });
+    list.appendChild(li);
+}
+
+// Clears all itmes from a given HTML list
+function clearList(list) {
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+}
+
+// Customer popup form
+function openCustomerForm(customer) {
+    document.getElementById("name").innerHTML = customer.name;
+    document.getElementById("cn").innerHTML = customer.contactNumber;
+    document.getElementById("ea").innerHTML = customer.email;
+    document.getElementById("add").innerHTML = customer.address;
+    document.getElementById("sub").innerHTML = customer.suburb;
+    document.getElementById("ci").innerHTML = customer.city;
+    document.getElementById("pc").innerHTML = customer.postCode;
+    document.getElementById("fa").innerHTML = customer.freshAir;
+    document.getElementById("n").innerHTML = customer.notes;
     document.getElementById("customerPopup").style.display = "block";
 }
 
@@ -88,14 +121,26 @@ function closeCustomerForm() {
     document.getElementById("customerPopup").style.display = "none";
 }
 
-function findCustomer(name) {
-    for (var i=0; i < customerData.length; i++) {
-        if (customerData[i].name == name) {
-            return customerData[i];
+// Finds a customer with a given property
+function findCustomer(property, value) {
+    if (property == "name") {
+        for (var i = 0; i < customerData.length; i++) {
+            if (customerData[i].name == value) {
+                return customerData[i];
+            }
         }
+    } else if (property == "id") {
+        for (var i = 0; i < customerData.length; i++) {
+            if (customerData[i].id == value) {
+                return customerData[i];
+            }
+        }
+    } else {
+        console.log("Invalid property search");
     }
 }
 
+// Dropbox button for call result
 function openDropButton() {
     document.getElementById("callResultDropdown").classList.toggle("show");
 }
@@ -116,41 +161,26 @@ window.onclick = function(event) {
 
 function setConfirmed() {
     let name = document.getElementById("name").innerHTML;
-    removeFromList(name, data1);
-    removeFromList(name, data2);
-    removeFromList(name, data3);
+    let customer = findCustomer("name", name);
 
-    data3.push(name);
-
-    setList(callList, data1);
-    setList(toBeConfirmed, data2);
-    setList(confirmed, data3);
+    customer.action = "confirmed";
+    setLists();
 }
 
 function setToBeConfirmed() {
     let name = document.getElementById("name").innerHTML;
-    removeFromList(name, data1);
-    removeFromList(name, data2);
-    removeFromList(name, data3);
+    let customer = findCustomer("name", name);
 
-    data2.push(name);
-
-    setList(callList, data1);
-    setList(toBeConfirmed, data2);
-    setList(confirmed, data3);
+    customer.action = "toBeConfirmed";
+    setLists();
 }
 
 function setCallBack() {
     let name = document.getElementById("name").innerHTML;
-    removeFromList(name, data1);
-    removeFromList(name, data2);
-    removeFromList(name, data3);
+    let customer = findCustomer("name", name);
 
-    data1.push(name);
-
-    setList(callList, data1);
-    setList(toBeConfirmed, data2);
-    setList(confirmed, data3);
+    customer.action = "callList";
+    setLists();
 }
 
 function removeFromList(item, list) {
