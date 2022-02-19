101d4eb58e3eb46d08fffa1d24691af24d8af5bd
Tim Hadler <timhadler93@gmail.com>
2022-02-19 21:16:25 +1300

    fixed call list

--- src/models/client-models.js
+++ src/models/client-models.js
@@ -5,7 +5,6 @@ exports.callList = async function () {
     try {
         const sqlQuery = "SELECT * from Clients";
         const rows = await db.query(sqlQuery);
-        
         return rows;
     } catch (error) {
         console.error(error.message);
