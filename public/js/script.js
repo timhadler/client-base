aeae748104db1e585bdb005f033e99d2e791c8c5
Tim Hadler <timhadler93@gmail.com>
2022-03-12 11:38:14 +1300

    rDate can be edit popup implemented, could use same tech for address etc

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,33 @@
 // Add buttons
-document.getElementById("addAddressBut").addEventListener('click', function() { addAddressForm()});
+document.getElementById("addAddressBut").addEventListener('click', function() { addAddressForm() });
 
 //Edit buttons
-//const editBut = document.getElementById("editAddressBut");
-//editBut.addEventListener('click', function() { editAddressForm()});
-document.getElementById("editClientButton").addEventListener('click', function() { editClientForm()});
+document.getElementById("editClientButton").addEventListener('click', function() { editClientForm() });
+
+const rDateEditButs = document.getElementsByName("rDateEditButton");
+for (let i = 0; i< rDateEditButs.length; i++) {
+    rDateEditButs[i].addEventListener('click', function() { rDateForm(rDateEditButs[i].id); });
+};
+
+const rDateCancelButs = document.getElementsByName("rDateCancelPopupButton");
+for (let i = 0; i< rDateCancelButs.length; i++) {
+    rDateCancelButs[i].addEventListener('click', function() { rDateCloseForm(rDateCancelButs[i].id); });
+};
 
 // Functions
-// function editAddressForm() {
-//     document.getElementById("editFormPopup").style.display = "block";
-// }
-
 function addAddressForm() {
     document.getElementById("addFormPopup").style.display = "block";
 }
 
 function editClientForm() {
     document.getElementById("cliFormPopup").style.display = "block";
+}
+
+function rDateForm(i) {
+    //console.log(i);
+    document.getElementById("rDatePopup-" + i).style.visibility = "visible";
+}
+
+function rDateCloseForm(i) {
+    document.getElementById("rDatePopup-" + i).style.visibility = "hidden";
 }
