68149de9c7d126b51eacd5622700f319537f206f
Tim Hadler <timhadler93@gmail.com>
2022-03-12 12:41:21 +1300

    edit popup works for contacts

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,12 @@
 // Add buttons
 document.getElementById("addAddressBut").addEventListener('click', function() { addAddressForm() });
 
-//Edit buttons
+/***********************************************************
+ * Edit buttons
+ ***********************************************************/
 document.getElementById("editClientButton").addEventListener('click', function() { editClientForm() });
 
+// rDate popup
 const rDateEditButs = document.getElementsByName("rDateEditButton");
 for (let i = 0; i< rDateEditButs.length; i++) {
     rDateEditButs[i].addEventListener('click', function() { rDateForm(rDateEditButs[i].id); });
@@ -12,6 +15,17 @@ for (let i = 0; i< rDateEditButs.length; i++) {
 const rDateCancelButs = document.getElementsByName("rDateCancelPopupButton");
 for (let i = 0; i< rDateCancelButs.length; i++) {
     rDateCancelButs[i].addEventListener('click', function() { rDateCloseForm(rDateCancelButs[i].id); });
+};
+
+// Contact popup
+const contactEditButs = document.getElementsByName("contactEditButton");
+for (let i = 0; i< contactEditButs.length; i++) {
+    contactEditButs[i].addEventListener('click', function() { contactForm(contactEditButs[i].id); });
+};
+
+const contactCancelButs = document.getElementsByName("contactCancelPopupButton");
+for (let i = 0; i< contactCancelButs.length; i++) {
+    contactCancelButs[i].addEventListener('click', function() { contactCloseForm(contactCancelButs[i].id); });
 };
 
 // Functions
@@ -30,4 +44,13 @@ function rDateForm(i) {
 
 function rDateCloseForm(i) {
     document.getElementById("rDatePopup-" + i).style.visibility = "hidden";
+}
+
+function contactForm(i) {
+    //console.log(i);
+    document.getElementById("contactPopup-" + i).style.visibility = "visible";
+}
+
+function contactCloseForm(i) {
+    document.getElementById("contactPopup-" + i).style.visibility = "hidden";
 }
