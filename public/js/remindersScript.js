e8eebe41d7c9f208eb75da2f72612c2dec15f444
Tim Hadler <timhadler93@gmail.com>
2023-06-25 12:49:51 +1200

    Checkboxes functionality in reminders list, if any checkbox is checked will reveal set status button, mutli set status function not redone yet

--- public/js/remindersScript.js
+++ public/js/remindersScript.js
@@ -7,6 +7,20 @@ document.getElementById("completedTabLink").addEventListener('click', (event) =>
 document.getElementById('actions').style.display = 'flex';
 document.getElementById('actionsTabLink').classList.add('active');
 
+// Sellect all Checkboxes
+document.getElementById("CLCheckbox").addEventListener('change', function() { checkAll(this) });
+document.getElementById("TBCCheckbox").addEventListener('change', function() { checkAll(this) });
+
+// Client button checkboxes
+const checkboxes = document.getElementsByName("selectedClients");
+checkboxes.forEach(function(checkbox) {
+  checkbox.addEventListener('change', revealStatusButton);
+});
+
+// Set status button
+document.getElementById("setStatusButton").addEventListener('click', function() { setStatusPopup() });
+document.getElementById("setStatusCloseButton").addEventListener('click', function() { setStatusClose() });
+
 // Reminder popup list buttons
 const clientButs = document.getElementsByName("clientCallButton");
 for (let i = 0; i< clientButs.length; i++) {
@@ -48,4 +62,72 @@ function reminderPopup(i) {
 function reminderPopupClose(i) {
   document.getElementById("clientPopup-" + i).style.display = "none";
   document.getElementById("overlay").style.visibility = "hidden";
+}
+
+// Reveals set multi status button if any checkboxes are selected
+function revealStatusButton() {
+  const checkboxes = document.getElementsByName("selectedClients");
+  let anyChecked = false;
+
+  checkboxes.forEach(function(checkbox) {
+    if (checkbox.checked) {
+      console.log("jd")
+      anyChecked = true;
+    }
+  });
+
+  if (anyChecked) {
+    document.getElementById("setStatusDiv").style.display = 'block';
+  } else {
+    document.getElementById("setStatusDiv").style.display = 'none';
+  }
+}
+
+// Set status popup
+function setStatusPopup() {
+  document.getElementById("setStatusPopup").style.visibility = "visible";
+  overlay.style.visibility = "visible";
+}
+
+function setStatusClose() {
+  document.getElementById("setStatusPopup").style.visibility = "hidden";
+  overlay.style.visibility = "hidden";
+}
+
+// Checks all list items associated with a select all checkbox
+function checkAll(cb) {
+  var clients = document.getElementsByName("selectedClients");
+  var list;
+  var checkStatus;
+
+  // Find which parent checkbox has been checked
+  if (cb.id == "CLCheckbox") { list = "cList" }
+  else if (cb.id = "TBCCheckbox") { list = "tbcList" }
+
+  // Has it been checked or unchecked?
+  if (cb.checked) {
+      checkStatus = true;
+  } else {
+      checkStatus = false;
+  }
+
+  // Check or uncheck the appropriate checkboxes
+  for (let i = 0; i < clients.length; i++) {
+      if (isChildOf(clients[i], list)) {
+          clients[i].checked = checkStatus;
+      }
+  }
+  revealStatusButton();
+}
+
+// Helper, checks if el is a child element of an element with a given id
+// Returns true or false if no parent is found with the given id
+function isChildOf(el, id) {
+  while (el && el.parentElement) {
+      el = el.parentElement;
+      if (el.id == id) {
+          return true;
+      }
+  }
+  return false;
 }
