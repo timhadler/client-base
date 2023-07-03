e27913bac867c5850a22b46faba6b5741968286f
Tim Hadler <timhadler93@gmail.com>
2023-07-03 21:16:29 +1200

    Fixed setStatus popup delete bug

--- public/js/remindersScript.js
+++ public/js/remindersScript.js
@@ -280,15 +280,11 @@ function revealStatusButton() {
 
 // Set status popup
 function setStatusPopup() {
-  // document.getElementById("setStatusPopup").style.visibility = "visible";
-  // overlay.style.visibility = "visible";
   $('#setStatusPopup').css('visibility', 'visible');
   $('#overlay').css('visibility', 'visible');
 }
 
 function setStatusClose() {
-  // document.getElementById("setStatusPopup").style.visibility = "hidden";
-  // overlay.style.visibility = "hidden";
   $('#setStatusPopup').css('visibility', 'hidden');
   $('#overlay').css('visibility', 'hidden');
 }
