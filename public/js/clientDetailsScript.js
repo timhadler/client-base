a9cfc96a61715f24ce8d5c0e26d290f0115b4c7f
Tim Hadler <timhadler93@gmail.com>
2023-08-11 22:08:37 +1200

    Clients page details popups now work

--- public/js/clientDetailsScript.js
+++ public/js/clientDetailsScript.js
@@ -60,103 +60,84 @@ for (let i = 0; i< contactCancelButs.length; i++) {
  ***********************************************************/
 // Add address popup
 function addAddressForm() {
-    const popup = document.getElementById("addAddressPopup");
-
-    popup.style.visibility = "visible";
+    document.getElementById("addAddressPopup").style.display = "block";
     document.getElementById("overlay").style.visibility = "visible";
 }
 
 function addAddressCloseForm() {
-    document.getElementById("addAddressPopup").style.visibility = "hidden";
+    document.getElementById("addAddressPopup").style.display = "none";
     document.getElementById("overlay").style.visibility = "hidden";
 }
 
 // Edit address popup
 function editAddressForm(i) {
-    const popup = document.getElementById("addressEditPopup-" + i);
-
-    popup.style.visibility = "visible";
+    document.getElementById("addressEditPopup-" + i).style.display = "block";
     popup.style.width = popup.parentNode.parentElement.clientWidth.toString() + "px";
 }
 
 function editAddressCloseForm(i) {
-    document.getElementById("addressEditPopup-" + i).style.visibility = "hidden";
+    document.getElementById("addressEditPopup-" + i).style.display = "none";
 }
 
 // Edit client popup
 function editClientForm() {
-    const popup = document.getElementById("cliFormPopup");
-
-    popup.style.visibility = "visible";
+    document.getElementById("cliFormPopup").style.display = "block";
     document.getElementById("overlay").style.visibility = "visible";
 }
 
 function editClientCloseForm() {
-    document.getElementById("cliFormPopup").style.visibility = "hidden";
+    document.getElementById("cliFormPopup").style.display = "none";
     document.getElementById("overlay").style.visibility = "hidden";
 }
 
 // Edit Comments popup
 function editCommentForm() {
-    const popup = document.getElementById("commentEditPopup");
-
-    popup.style.visibility = "visible";
+    document.getElementById("commentEditPopup").style.display = "block";
 }
 
 function editCommentCloseForm() {
-    document.getElementById("commentEditPopup").style.visibility = "hidden";
+    document.getElementById("commentEditPopup").style.display = "none";
 }
 
 // Add date popup
-function addDateForm(i) {
-    const popup = document.getElementById("addCallPopup");
-
-    popup.style.visibility = "visible";
-
+function addDateForm() {
+    document.getElementById("addCallPopup").style.display = "block";
     document.getElementById("overlay").style.visibility = "visible";
 }
 
-function addDateCloseForm(i) {
-    document.getElementById("addCallPopup").style.visibility = "hidden";
+function addDateCloseForm() {
+    document.getElementById("addCallPopup").style.display = "none";
     document.getElementById("overlay").style.visibility = "hidden";
 }
 
 // Edit date popup
 function editDateForm(i) {
-    const popup = document.getElementById("rDateEditPopup-" + i);
-
-    popup.style.visibility = "visible";
-
+    document.getElementById("rDateEditPopup-" + i).style.display = "block";
     document.getElementById("overlay").style.visibility = "visible";
 }
 
 function editDateCloseForm(i) {
-    document.getElementById("rDateEditPopup-" + i).style.visibility = "hidden";
+    document.getElementById("rDateEditPopup-" + i).style.display = "none";
     document.getElementById("overlay").style.visibility = "hidden";
 }
 
 // Add contact popup
-function addContactForm(i) {
-    const popup = document.getElementById("addContactPopup");
-
-    popup.style.visibility = "visible";
+function addContactForm() {
+    document.getElementById("addContactPopup").style.display = "block";
     document.getElementById("overlay").style.visibility = "visible";
 }
 
-function addContactCloseForm(i) {
-    document.getElementById("addContactPopup").style.visibility = "hidden";
+function addContactCloseForm() {
+    document.getElementById("addContactPopup").style.display = "none";
     document.getElementById("overlay").style.visibility = "hidden";
 }
 
 // Edit contact popup
 function editContactForm(i) {
-    //console.log("here");
-    const popup = document.getElementById("contactEditPopup-" + i);
-
-    popup.style.visibility = "visible";
+    document.getElementById("contactEditPopup-" + i).style.display = "block";
     popup.style.width = popup.parentNode.parentElement.clientWidth.toString() + "px";
 }
 
 function editContactCloseForm(i) {
-    document.getElementById("contactEditPopup-" + i).style.visibility = "hidden";
+    document.getElementById("contactEditPopup-" + i).style.display = "none";
 }
