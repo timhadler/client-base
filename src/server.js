da701e5a62bcd2ac9c70382567cb0214d34de3e6
Tim <timhadler93@gmail.com>
2025-11-01 14:09:18 +1100

    removed all prev styleing files in prep for overhaul. Added login page html

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ const authRouter = require("./routes/auth");
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
-app.use(expressLayouts);
+//app.use(expressLayouts);
 app.use(express.static(path.join("public")));
 app.use(override("_method"));
 app.use(bodyParser.urlencoded( {extended: false } ));
